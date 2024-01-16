b3c033f7480b14dc9b6fceee42f4f72592adf0bc
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-01-15 21:21:15 -0800

    Initial Commit

---
+++ Lesson4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson4
+//
+//  Created by NSPC201admin on 2024/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
