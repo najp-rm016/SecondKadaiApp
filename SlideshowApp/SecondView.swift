1986549a68af73c744079947ab639093ce627d66
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-02-29 21:22:37 -0800

    初期設定のコードから画像を３枚切り替えられるようにした ただ汎用性は低い

---
+++ SlideshowApp/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  SlideshowApp
+//
+//  Created by NSPC201admin on 2024/01/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
